36facde84eb954458ca0992f9619e282513a5704
TesliukK <teslukkirill@gmail.com>
2022-11-01 12:39:55 +0200

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,41 @@
+let s1 = 'hello'
+console.log(s1)
+let s2 = 'owu'
+console.log(s2)
+let s3 = 'com'
+console.log(s3)
+let s4 = 'ua'
+console.log(s4)
+
+let num1 = 1
+console.log(num1)
+let num2 = 10
+console.log(num2)
+let num3 = -999
+console.log(num3)
+let num4 = 123
+console.log(num4)
+
+const PI = 3.14
+console.log(PI)
+let num5 = 2.7
+console.log(num5)
+let num6 = 16
+console.log(num6)
+
+let b1 = 10 > 5
+console.log(b1)
+let b2 = 10 < 5
+console.log(b2)
+
+let firstName = 'Tesliuk'
+let middleName = 'Kyrylo'
+let lastName = 'Serhiovych'
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
