959fac99fa833dca3b8308178e514c69addf3d0e
TesliukK <teslukkirill@gmail.com>
2022-11-03 12:54:49 +0200

    done

---
+++ lesson2/main.js
@@ -0,0 +1,77 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let arr = [1, 'qwe', 123, 'asd', true, 3.14, 321, false, 987, [11, 22, 33]]
+console.log(arr)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let books1 = {
+    title: 'book',
+    pageCount: 150,
+    genre: 'fiction'
+}
+console.log(books1)
+let books2 = {
+    title: 'book',
+    pageCount: 300,
+    genre: 'documentary'
+}
+console.log(books2)
+let books3 = {
+    title: 'book',
+    pageCount: 400,
+    genre: 'historical'
+}
+console.log(books3)
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+let book1 = {
+    title: 'book',
+    pageCount: 150,
+    genre: 'fiction',
+    authors: {
+        name: 'dima',
+        age: 35
+    }
+}
+console.log(book1)
+let book2 = {
+    title: 'book',
+    pageCount: 350,
+    genre: 'fiction',
+    authors: {
+        name: 'anna',
+        age: 25
+    }
+}
+console.log(book2)
+let book3 = {
+    title: 'book',
+    pageCount: 550,
+    genre: 'fiction',
+    authors: {
+        name: 'kolya',
+        age: 45
+    }
+}
+console.log(book3)
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let user = [
+    {name: 'inna', username: 'qweasd', password: 'qwe12345'},
+    {name: 'vasya', username: 'qweasd', password: 'asd1234'},
+    {name: 'vanya', username: 'qweasd', password: 'zxc2345'},
+    {name: 'tolya', username: 'qweasd', password: 'cvb2345'},
+    {name: 'anna', username: 'qweasd', password: '4561234'},
+    {name: 'vika', username: 'qweasd', password: 'sdgh2345'},
+    {name: 'lena', username: 'qweasd', password: 'sgh215424'},
+    {name: 'vova', username: 'qweasd', password: 'sghj345637'},
+    {name: 'anton', username: 'qweasd', password: 'dgjgk2347'},
+    {name: 'andriy', username: 'qweasd', password: '234lgfg'}
+]
+console.log(user[0].password);
+console.log(user[1].password);
+console.log(user[2].password);
+console.log(user[3].password);
+console.log(user[4].password);
+console.log(user[5].password);
+console.log(user[6].password);
+console.log(user[7].password);
+console.log(user[8].password);
+console.log(user[9].password);
